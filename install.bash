3f152284beda89e6ed4b484e391271fd51f61484
Kptainflintt <67273050+Kptainflintt@users.noreply.github.com>
2022-10-24 15:10:49 +0200

    Update install.bash

--- install.bash
+++ install.bash
@@ -8,7 +8,8 @@ apt-get install docker.io git nmap net-tools -y
 
 #Installing Docker-compose
 wget https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-linux-x86_64
-cp docker-compose-linux-x86_64 /bin/bash/docker-compose
+mv docker-compose-linux-x86_64 docker-compose
+cp docker-compose /bin/bash/
 chmod +x /bin/bash/docker-compose
 
 #Pulling and compose required containers 

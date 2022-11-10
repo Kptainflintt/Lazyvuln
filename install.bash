f1745fa138a9c93050604fd830c1e5f40ce57bb3
Kptainflintt <67273050+Kptainflintt@users.noreply.github.com>
2022-11-10 09:48:42 +0100

    Update install.bash

--- install.bash
+++ install.bash
@@ -4,7 +4,8 @@
 apt-get update && apt-get upgrade -y
 
 #Installing required components
-apt-get install docker.io git nmap net-tools -y
+apt-get install docker.io git nmap net-tools python-pip -y
+pip install faraday-cli
 
 #Installing Docker-compose
 wget https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-linux-x86_64
@@ -16,13 +17,43 @@ chmod +x /bin/docker-compose
 docker pull kptainflintt/lazyvuln
 wget https://raw.githubusercontent.com/infobyte/faraday/master/docker-compose.yaml
 echo "Building Faraday Stack (it can take some time...)"
-docker-compose up &> faraday.txt &
-sleep 20
+docker-compose up &> faraday.txt 
 echo "Done..."
+cat faraday.txt | grep "Admin user" | cut -d " " 
 docker kill $(docker ps -q)
 docker container prune -f
 
-#Cleaning 
-rm docker-compose
-echo "Done, you can run scans!"
+#downloading scan script
+wget https://raw.githubusercontent.com/Kptainflintt/Lazyvuln/master/start-scan
+chmod +x start-scan
+cp start-scan /usr/bin
 
+
+#Cleaning 
+rm docker-compose*
+rm start-scan
+echo "Done, you can run scans! Just execute "start-scan" "
+
+#Personalize
+read -p 'Would you like to perform scheduled scans? (y/n) ' schedule
+
+if [ "$schedule" = "y"]
+	then
+		echo "How often?"
+		echo "1. Every day"
+		echo "2. Every week"
+		echo "3. Every month"
+		read cron
+		
+		if [ "$cron" = 1 ]
+			then
+				cp start-scan /etc/cron.daily/start-scan
+		elif [ "$cron"= 2 ]
+			then 
+				cp start-scan /etc/cron.weekly/start-scan
+		elif [ "$cron"= 3 ]
+			then 
+				cp start-scan /etc/cron.monthly/start-scan
+		fi
+		
+echo "You're done, have nice scans ;) "
